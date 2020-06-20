a1e4d9661f0e951549c9c5e008827ad206fde176
jtraviscoates <coatesjt@hotmail.com>
2020-06-20 14:29:42 -0500

    Create ad an event form

---
+++ ViewModels/GigFormViewModel.cs
@@ -0,0 +1,14 @@
+﻿using AlongSide.Models;
+using System.Collections.Generic;
+
+namespace AlongSide.ViewModels
+{
+    public class GigFormViewModel
+    {
+        public string Venue { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+        public int Profession { get; set; }//changed name here and Create View (from Genre)
+        public IEnumerable<Genre> Genres { get; set; }
+    }
+}
