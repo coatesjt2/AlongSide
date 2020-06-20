7c0c87e3d26732d99d83845064a1e25e9d598a7f
jtraviscoates <coatesjt@hotmail.com>
2020-06-20 12:52:27 -0500

    Create Gig Model

---
+++ Models/Genre.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AlongSide.Models
+{
+    public class Genre
+    //mentors background or type of Professional
+    {
+        public byte Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+    }
+}
