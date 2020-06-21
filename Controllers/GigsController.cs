e9c5faad5e64b22525caf823bb808f835929b442
jtraviscoates <coatesjt@hotmail.com>
2020-06-20 20:48:39 -0500

    Implement anti-forgery token

--- Controllers/GigsController.cs
+++ Controllers/GigsController.cs
@@ -27,6 +27,7 @@ namespace AlongSide.Controllers
 
         [Authorize]
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public ActionResult Create(GigFormViewModel viewModel)
         {
 
