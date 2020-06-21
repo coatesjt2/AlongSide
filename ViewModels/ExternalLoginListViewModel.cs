afc3a97fd622437ecbab1e614cf76eced9323258
jtraviscoates <coatesjt@hotmail.com>
2020-06-20 23:21:19 -0500

    Moved AccountVewModels to ViewModel classes.

---
+++ ViewModels/ExternalLoginListViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace AlongSide.ViewModels
+{
+    public class ExternalLoginListViewModel
+    {
+        public string ReturnUrl { get; set; }
+    }
+}
---
+++ ViewModels/ForgotViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AlongSide.ViewModels
+{
+    public class ForgotViewModel
+    {
+        [Required]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+    }
+}
