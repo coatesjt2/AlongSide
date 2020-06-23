e3a885f7479c70ab51e53ace86b4a6cc16dd3ff7
jtraviscoates <coatesjt@hotmail.com>
2020-06-22 19:41:57 -0500

    Implement Upcoming Events Page

--- Controllers/GigsController.cs
+++ Controllers/GigsController.cs
@@ -1,6 +1,7 @@
 ﻿using AlongSide.Models;
 using AlongSide.ViewModels;
 using Microsoft.AspNet.Identity;
+using System;
 using System.Data.Entity;
 using System.Linq;
 using System.Web.Mvc;
@@ -14,6 +15,19 @@ namespace AlongSide.Controllers
         {
             _context = new ApplicationDbContext();
         }
+
+        [Authorize]
+        public ActionResult Mine()
+        {
+            var userId = User.Identity.GetUserId();
+            var gigs = _context.Gigs
+                .Where(g => g.ArtistId == userId && g.DateTime > DateTime.Now)
+                .Include(g => g.Genre)
+                .ToList();
+
+            return View(gigs);
+        }
+
 
         [Authorize]
         public ActionResult Attending()
@@ -70,7 +84,7 @@ namespace AlongSide.Controllers
             _context.Gigs.Add(gig);
             _context.SaveChanges();
 
-            return RedirectToAction("Index", "Home");
+            return RedirectToAction("Mine", "Gigs");
         }
     }
 }
